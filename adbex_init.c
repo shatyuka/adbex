26de8a65a7b541cff48854b78fe4f7c19fc14c16
shatyuka <shatyuka@qq.com>
2025-10-16 16:56:22 +0800

    Fix compatibility with Magic Mount

--- adbex_init.c
+++ adbex_init.c
@@ -9,14 +9,13 @@
 
 #define ADBD_PATH_APEX "/apex/com.android.adbd/bin/adbd"
 #define ADBD_PATH "/system/bin/adbd"
-
-char adbex_adbd[PATH_MAX];
+#define ADBDEX_ADBD "/data/adb/adbex/libadbex_adbd.so"
 
 int (*orig_execv)(const char* path, char* const argv[]);
 int hook_execv(const char* path, char* const argv[]) {
   if (unlikely(!strcmp(path, ADBD_PATH_APEX) || !strcmp(path, ADBD_PATH))) {
     klog(LOG_TAG, "adbd pid: %d\n", getpid());
-    setenv("LD_PRELOAD", adbex_adbd, 1);
+    setenv("LD_PRELOAD", ADBDEX_ADBD, 1);
     return orig_execv(path, argv);
   }
   return orig_execv(path, argv);
@@ -24,15 +23,6 @@ int hook_execv(const char* path, char* const argv[]) {
 
 CONSTRUCTOR UNUSED void adbex_init_main() {
   klog(LOG_TAG, "injected into init");
-
-  const char* adbex_path;
-  if (access("/sbin/adbex", F_OK) == 0) {
-    adbex_path = "/sbin/adbex";
-  } else {
-    adbex_path = "/debug_ramdisk/adbex";
-  }
-  snprintf(adbex_adbd, sizeof(adbex_adbd), "%s/libadbex_adbd.so", adbex_path);
-  klog(LOG_TAG, "adbex_adbd: %s", adbex_adbd);
 
   plthook_t* plthook;
   int err = plthook_open_by_address(&plthook, (void*)get_module_base(1, "/init\n"));
--- package/post-fs-data.sh
+++ package/post-fs-data.sh
@@ -2,10 +2,14 @@
 MODDIR=${0%/*}
 cd "$MODDIR" || exit
 
-ADBEX_PATH=/sbin/adbex
-[ -d /sbin ] || ADBEX_PATH=/debug_ramdisk/adbex
-
+ADBEX_PATH=/data/adb/adbex
 mkdir -p $ADBEX_PATH
+
+if [ -f /linkerconfig/com.android.adbd/ld.config.txt ]; then
+    echo "# adbex" >> /linkerconfig/com.android.adbd/ld.config.txt
+    echo "namespace.default.permitted.paths += $ADBEX_PATH" >> /linkerconfig/com.android.adbd/ld.config.txt
+fi
+
 cp lib64/libadbex_init.so $ADBEX_PATH
 cp lib64/libadbex_adbd.so $ADBEX_PATH
 chcon -R u:object_r:system_file:s0 $ADBEX_PATH
